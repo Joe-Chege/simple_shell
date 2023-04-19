d5e927700be163b9bdeefed6bc3f58f5d18701eb
Joe-Chege <njorogejosephchege@gmail.com>
2023-04-19 07:34:39 -0700

    getline

---
+++ getline.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *line = NULL; 
+	size_t bufsize = 0;
+	ssize_t characters;
+
+	printf("$ "); 
+
+	characters = getline(&line, &bufsize, stdin); 
+
+	if (characters == -1) 
+	{
+		perror("getline");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("You entered: %s", line);
+
+	free(line); 
+
+	return 0;
+}
---
+++ printac.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	(void)argc; 
+
+	while (argv[i] != NULL)
+	{
+		printf("Argument %d: %s\n", i + 1, argv[i]);
+		i++;
+	}
+
+	return 0;
+}
