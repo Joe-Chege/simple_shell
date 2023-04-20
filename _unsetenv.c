9c8782c684290dca2c75cc0c4666c76e69fdbfef
Joe-Chege <njorogejosephchege@gmail.com>
2023-04-20 02:19:12 -0700

    Finally it has compiled

--- _unsetenv.c
+++ _unsetenv.c
@@ -1,38 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 /**
- * _unsetenv - Deletes an environment variable
- * @name: Name of the environment variable to delete
+ * _unsetenv - Delete a variable from the environment
+ * @name: The name of the variable to delete
  *
  * Return: 0 on success, -1 on failure
  */
 int _unsetenv(const char *name)
 {
-    if (name == NULL)
+    
+    if (name == NULL || *name == '\0')
         return -1;
 
-    size_t name_len = strlen(name);
-    char **env = environ;
-
     
+    extern char **environ;
+    char **env = environ;
     while (*env != NULL)
     {
-        if (strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
+        
+        if (strncmp(*env, name, strlen(name)) == 0 && (*env)[strlen(name)] == '=')
         {
             
-            char **ptr = env;
-            while (*ptr != NULL)
-            {
-                *ptr = *(ptr + 1);
-                ptr++;
-            }
+            for (char **p = env; *p != NULL; p++)
+                *p = *(p + 1);
             return 0;
         }
         env++;
     }
+
+    
+    return -1;
+}
+
+int main(void)
+{
+    // Example usage of _unsetenv function
+    printf("Before unset:\n");
+    printf("MY_VARIABLE1=%s\n", getenv("MY_VARIABLE1"));
+    printf("MY_VARIABLE2=%s\n", getenv("MY_VARIABLE2"));
+
+    // Delete a variable from the environment
+    _unsetenv("MY_VARIABLE1");
+
+    printf("After unset:\n");
+    printf("MY_VARIABLE1=%s\n", getenv("MY_VARIABLE1"));
+    printf("MY_VARIABLE2=%s\n", getenv("MY_VARIABLE2"));
 
     return 0;
 }
