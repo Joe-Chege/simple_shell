e2996ee939aea7bc24a12d7cf872beda0671ce7c
mitchellshera <mitchellwachera@gmail.com>
2023-04-21 17:50:54 +0300

    Update pid.c

--- pid.c
+++ pid.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
-
 /**
- * main - PID
+ * main_pid - Print the current process ID (PID)
  *
  * Return: Always 0.
  */
+
 int main_pid(void)
 {
     pid_t my_pid;
