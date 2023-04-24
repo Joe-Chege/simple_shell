280b4ecf84f6ccf1faf3bc8572475f29aa8d8205
mitchellshera <mitchellwachera@gmail.com>
2023-04-24 17:42:02 +0300

    new files

--- shell.h
+++ shell.h
@@ -1,68 +1,73 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/wait.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <signal.h>
-
-
-
-
-/* Function prototypes */
-int main_getenv(void);
-int main_fork(void); /* Updated function name */
-int main_pathdir(void);
-int main_pid(void);
-int main_ppid(void);
-int printac(int argc, char *argv[]);
-int printdir(void);
-void print_path_directories(void);
-int main_printpath(void);
-int set_env(void);
-char *_strtok(char *str, const char *delim);
-int _unsetenv(const char *name);
-int main_unsetenv (void);
-int main_strok(void);
-
-typedef int pid_t;
-
-
-
-int main(void)
+/**
+ * struct variables - variables
+ * @av: command line arguments
+ * @buffer: buffer of command
+ * @env: environment variables
+ * @count: count of commands entered
+ * @argv: arguments at opening of shell
+ * @status: exit status
+ * @commands: commands to execute
+ */
+typedef struct variables
 {
-    char *line = NULL;
-    size_t bufsize = 0;
-    ssize_t characters;
+	char **av;
+	char *buffer;
+	char **env;
+	size_t count;
+	char **argv;
+	int status;
+	char **commands;
+} vars_t;
 
-    printf("$ ");
+/**
+ * struct builtins - struct for the builtin functions
+ * @name: name of builtin command
+ * @f: function for corresponding builtin
+ */
+typedef struct builtins
+{
+	char *name;
+	void (*f)(vars_t *);
+} builtins_t;
 
-    characters = getline(&line, &bufsize, stdin);
-
-    if (characters == -1)
-    {
-        perror("getline");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("You entered: %s", line);
-
-    free(line);
-
-    return 0;
-}
-
+typedef struct {
+    char *name;
+    char *value;
+} Alias;
 
 
+char **make_env(char **env);
+void free_env(char **env);
 
-#endif
+ssize_t _puts(char *str);
+char *_strdup(char *strtodup);
+int _strcmpr(char *strcmp1, char *strcmp2);
+char *_strcat(char *strc1, char *strc2);
+unsigned int _strlen(char *str);
+
+char **tokenize(char *buffer, char *delimiter);
+char **_realloc(char **ptr, size_t *size);
+char *new_strtok(char *str, const char *delim);
+
+void (*check_for_builtins(vars_t *vars))(vars_t *vars);
+void new_exit(vars_t *vars);
+void _env(vars_t *vars);
+void new_setenv(vars_t *vars);
+void new_unsetenv(vars_t *vars);
+
+void add_key(vars_t *vars);
+char **find_key(char **env, char *key);
+char *add_value(char *key, char *value);
+int _atoi(char *str);
+
+void check_for_path(vars_t *vars);
+int path_execute(char *command, vars_t *vars);
+char *find_path(char **env);
+int execute_cwd(vars_t *vars);
+int check_for_dir(char *str);
+
+void print_error(vars_t *vars, char *msg);
+void _puts2(char *str);
+char *_uitoa(unsigned int count);
+
+#endif /* MAIN_H_ */
