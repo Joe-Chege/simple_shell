ffaaf90ea97efac9b51b9e095ac9e405a7ad3dd3
mitchellshera <mitchellwachera@gmail.com>
2023-04-24 17:46:16 +0300

    fix shell.h

--- shell.h
+++ shell.h
@@ -1,3 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+
 /**
  * struct variables - variables
  * @av: command line arguments
