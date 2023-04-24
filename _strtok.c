c4b5cf6775d4cf3fc2aa10d742527b1380a9ffe1
mitchellshera <mitchellwachera@gmail.com>
2023-04-24 17:37:08 +0300

    token

--- _strtok.c
+++ _strtok.c
@@ -1,42 +1,66 @@
 #include "shell.h"
-
 /**
- * _strtok - Splits a string into an array of words.
- * @str: The string to be split.
- * @delim: The delimiter used to split the string.
+ * check_match - checks if a character matches any in a string
+ * @c: character to check
+ * @str: string to check
  *
- * Return: A pointer to the first token found in the string.
+ * Return: 1 if match, 0 if not
  */
-char *_strtok(char *str, const char *delim)
+unsigned int check_match(char c, const char *str)
 {
-    static char *last_token = NULL;
-    char *token = NULL;
+	unsigned int i;
 
-    if (str != NULL)
-        last_token = str;
-
-    if (last_token == NULL)
-        return NULL;
-
-    token = strtok(last_token, delim);
-    last_token = (token != NULL) ? token + strlen(token) : NULL;
-
-    return token;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (c == str[i])
+			return (1);
+	}
+	return (0);
 }
 
-int main_strok(void)
+/**
+ * new_strtok - custom strtok
+ * @str: string to tokenize
+ * @delim: delimiter to tokenize against
+ *
+ * Return: pointer to the next token or NULL
+ */
+char *new_strtok(char *str, const char *delim)
 {
-    char str[] = "Hello world! How are you?";
-    const char delim[] = " !?";
-    char *token = NULL;
+	static char *token_start;
+	static char *next_token;
+	unsigned int i;
 
-    printf("Original string: %s\n", str);
-    printf("Words:\n");
-
-    while ((token = _strtok(token == NULL ? str : NULL, delim)) != NULL)
-    {
-        printf("%s\n", token);
-    }
-
-    return 0;
+	if (str != NULL)
+		next_token = str;
+	token_start = next_token;
+	if (token_start == NULL)
+		return (NULL);
+	for (i = 0; next_token[i] != '\0'; i++)
+	{
+		if (check_match(next_token[i], delim) == 0)
+			break;
+	}
+	if (next_token[i] == '\0' || next_token[i] == '#')
+	{
+		next_token = NULL;
+		return (NULL);
+	}
+	token_start = next_token + i;
+	next_token = token_start;
+	for (i = 0; next_token[i] != '\0'; i++)
+	{
+		if (check_match(next_token[i], delim) == 1)
+			break;
+	}
+	if (next_token[i] == '\0')
+		next_token = NULL;
+	else
+	{
+		next_token[i] = '\0';
+		next_token = next_token + i + 1;
+		if (*next_token == '\0')
+			next_token = NULL;
+	}
+	return (token_start);
 }
--- shell.h
+++ shell.h
@@ -39,6 +39,28 @@ typedef int pid_t;
 
 
 
+int main(void)
+{
+    char *line = NULL;
+    size_t bufsize = 0;
+    ssize_t characters;
+
+    printf("$ ");
+
+    characters = getline(&line, &bufsize, stdin);
+
+    if (characters == -1)
+    {
+        perror("getline");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("You entered: %s", line);
+
+    free(line);
+
+    return 0;
+}
 
 
 
